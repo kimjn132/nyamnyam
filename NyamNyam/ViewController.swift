55da1291697203bbfda08f6799e140db1238e812
kimjn132 <kjhi8309@gmail.com>
2023-02-02 00:33:26 +0900

    first commit

---
+++ NyamNyam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NyamNyam
+//
+//  Created by Anna Kim on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
