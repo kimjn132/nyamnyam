fbd6f3aee411b2e48b805f9bc824319af89f8304
altese <readme42u@gmail.com>
2023-02-27 22:34:16 +0900

    리뷰 작성 페이지 추가

---
+++ NyamNyam/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  PFoodWish
+//
+//  Created by 박예진 on 2023/02/03.
+//
+
+import Foundation
+
+import Foundation
+
+struct Message{
+    static var check = ""
+    static var id: Int = 0
+    static var name: String = ""
+    static var image: Data?
+    static var address: String = ""
+    static var content: String = ""
+}
+
