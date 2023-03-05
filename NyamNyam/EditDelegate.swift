ddd0d1a5a621809efc1fe4b6f96555774bdf5309
javanyam <elyje5907@gmail.com>
2023-03-05 17:57:40 +0900

    wishList update 가능

---
+++ NyamNyam/EditDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  EditDelegate.swift
+//  NyamNyam
+//
+//  Created by 예띤 on 2023/03/05.
+//
+
+import Foundation
+
+protocol EditDelegate{
+    func didEditPart(_ controller : WishAddViewController)
+}
